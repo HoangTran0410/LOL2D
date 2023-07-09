106e3ebecc6b2400bf6b7ee5e967d92106836ca5
Hoang Tran <99.hoangtran@gmail.com>
2023-07-09 19:42:56 +0700

    update olaf Q

--- src/game/gameObject/spells/Olaf_Q.js
+++ src/game/gameObject/spells/Olaf_Q.js
@@ -1,16 +1,17 @@
 import AssetManager from '../../../managers/AssetManager.js';
 import VectorUtils from '../../../utils/vector.utils.js';
+import BuffAddType from '../../enums/BuffAddType.js';
 import Spell from '../Spell.js';
 import SpellObject from '../SpellObject.js';
 import Slow from '../buffs/Slow.js';
-import Ghost, { Ghost_Buff } from './Ghost.js';
+import { Ghost_Buff } from './Ghost.js';
 
 export default class Olaf_Q extends Spell {
   image = AssetManager.getAsset('spell_olaf_q');
   name = 'Phóng Rìu (Olaf_Q)';
   description =
-    'Ném rìu đến điểm chỉ định, gây 15 sát thương và làm chậm 40% trong 1s cho những kẻ địch nó đi qua, bạn cũng nhận được 30% tốc chạy trong 1s nếu ném trúng. Rìu tồn tại trong 5s, nếu nhặt được rìu, thời gian hồi chiêu sẽ được tái tạo.';
-  coolDown = 6000;
+    'Ném rìu đến điểm chỉ định, gây 15 sát thương và làm chậm 40% trong 1s cho những kẻ địch nó đi qua, bạn cũng nhận được 30% tốc chạy trong 1s nếu ném trúng. Rìu tồn tại trong 5s, nếu nhặt được rìu, thời gian hồi chiêu được giảm 60%.';
+  coolDown = 5000;
 
   maxThrowRange = 350;
   axeLifeTime = 5000;
@@ -67,6 +68,8 @@ export class Olaf_Q_Object extends SpellObject {
   update() {
     // flying phase
     if (this.phase === Olaf_Q_Object.PHASES.FLYING) {
+      this.angle += 0.2;
+
       VectorUtils.moveVectorToVector(this.position, this.destination, this.speed);
       if (this.position.dist(this.destination) < this.speed) {
         this.phase = Olaf_Q_Object.PHASES.WAIT_FOR_PICK_UP;
@@ -83,6 +86,7 @@ export class Olaf_Q_Object extends SpellObject {
 
       enemies.forEach(enemy => {
         let slowBuff = new Slow(1000, this.owner, enemy);
+        slowBuff.image = AssetManager.getAsset('spell_olaf_q');
         slowBuff.percent = 0.4;
         enemy.addBuff(slowBuff);
         enemy.takeDamage(this.damage, this.owner);
@@ -92,6 +96,8 @@ export class Olaf_Q_Object extends SpellObject {
       // speed up owner if hit
       if (enemies.length > 0) {
         let speedUpBuff = new Ghost_Buff(1000, this.owner, this.owner);
+        speedUpBuff.buffAddType = BuffAddType.RENEW_EXISTING;
+        speedUpBuff.image = AssetManager.getAsset('spell_olaf_q');
         speedUpBuff.percent = 0.3;
         this.owner.addBuff(speedUpBuff);
       }
@@ -115,23 +121,31 @@ export class Olaf_Q_Object extends SpellObject {
         //     spell.resetCoolDown();
         //   });
 
-        this.spellSource?.resetCoolDown?.();
+        this.spellSource.currentCooldown *= 0.4;
         this.toRemove = true;
       }
     }
   }
 
   draw() {
-    // flying phase
-    if (this.phase === Olaf_Q_Object.PHASES.FLYING) {
-      push();
-      fill(255, 0, 0);
-      circle(this.position.x, this.position.y, this.size);
-      pop();
-    }
+    // draw axe shape, with small rect is the handle, and 2 triangle is the blade
+    push();
+    noStroke();
+    fill(200, 150, 200);
+    translate(this.position.x, this.position.y);
+    rotate(this.angle);
+
+    let handleLength = this.size * 1.5;
+    let handleWidth = 10;
+    let bladeSize = 30;
+
+    rect(-handleLength, -handleWidth / 2, handleLength, handleWidth);
+    triangle(0, bladeSize / 2, bladeSize / 3, -bladeSize, -bladeSize, -bladeSize);
+    triangle(0, -bladeSize / 2, bladeSize / 3, bladeSize, -bladeSize, bladeSize);
+    pop();
 
     // wait for pick up phase
-    else if (this.phase === Olaf_Q_Object.PHASES.WAIT_FOR_PICK_UP) {
+    if (this.phase === Olaf_Q_Object.PHASES.WAIT_FOR_PICK_UP) {
       // draw pickup range
       push();
       fill(100, 30);
@@ -144,16 +158,6 @@ export class Olaf_Q_Object extends SpellObject {
         0
       );
       arc(this.position.x, this.position.y, this.pickupRange, this.pickupRange, 0, arcLength, PIE);
-      pop();
-
-      // draw axe shape, with small rect is the handle, and another big rect is the blade
-      push();
-      stroke(30);
-      fill(255, 0, 0, alpha);
-      translate(this.position.x, this.position.y);
-      rotate(this.angle);
-      rect(-this.size, -5, this.size, 10);
-      rect(-this.size / 2, -this.size / 2, this.size, this.size);
       pop();
     }
   }
--- src/utils/vector.utils.js
+++ src/utils/vector.utils.js
@@ -9,6 +9,14 @@ const VectorUtils = {
     let to = p5.Vector.add(from, p5.Vector.sub(targetVector, from).limit(maxRange));
     return { from, to };
   },
+  getVectorWithMinAndMaxRange(rootVector, targetVector, minRange, maxRange) {
+    let from = rootVector.copy();
+    let to = p5.Vector.add(from, p5.Vector.sub(targetVector, from).limit(maxRange));
+    if (from.dist(to) < minRange) {
+      to = p5.Vector.add(from, p5.Vector.sub(targetVector, from).setMag(minRange));
+    }
+    return { from, to };
+  },
   getVectorWithAngleAndRange(rootVector, angle, range) {
     let from = rootVector.copy();
     let to = p5.Vector.add(from, p5.Vector.fromAngle(angle).setMag(range));
