ecf356236673fb961c51f404abaee36204c8322d
hoang.tran12 <hoang.tran12@mservice.com.vn>
2023-07-12 16:22:04 +0700

    WIP

--- src/game/ObjectManager.js
+++ src/game/ObjectManager.js
@@ -1,7 +1,63 @@
 export default class ObjectManager {
+  objects = {};
+
   constructor(game) {
     this.game = game;
+  }
 
-    this.objects = [];
+  update() {
+    // update
+    for (let key in this.objects) {
+      this.objects[key].update();
+    }
+
+    // check remove
+    for (let key in this.objects) {
+      if (this.objects[key].toRemove) {
+        this.objects[key].onRemoved();
+        delete this.objects[key];
+      }
+    }
+  }
+
+  addObject(object) {
+    this.objects[object.id] = object;
+    object.onAdded();
+  }
+
+  removeObject(object) {
+    object.toRemove = true;
+  }
+
+  getObjects() {
+    return Object.values(this.objects);
+  }
+
+  getObjectsByType(type) {
+    return this.getObjects().filter(o => o instanceof type);
+  }
+
+  getObjectById(id) {
+    return this.objects[id];
+  }
+
+  getObjectsInRange({ position, radius, teamId, type, customFilter }) {
+    return this.getObjects().filter(o => {
+      if (o.toRemove) return false;
+      if (teamId && !(o.teamId === teamId)) return false;
+      if (type && !(o instanceof type)) return false;
+      if (o.position.dist(position) > radius) return false;
+      if (typeof customFilter === 'function' && !customFilter(o)) return false;
+      return true;
+    });
+  }
+
+  getAllObjectsForTeam(teamId, objectTypes = []) {
+    return this.getObjects().filter(o => {
+      if (o.toRemove) return false;
+      if (!(o.teamId === teamId)) return false;
+      if (objectTypes.length > 0 && !objectTypes.some(t => o instanceof t)) return false;
+      return true;
+    });
   }
 }
--- src/game/gameObject/attackableUnits/AttackableUnit.js
+++ src/game/gameObject/attackableUnits/AttackableUnit.js
@@ -3,8 +3,9 @@ import Stats from '../Stats.js';
 
 export default class AttackableUnit extends GameObject {
   stats = new Stats();
-  _buffEffectsToDisable = 0;
+  buffs = [];
   _buffEffectsToEnable = 0;
+  _buffEffectsToDisable = 0;
 
   constructor({
     game,
@@ -17,6 +18,38 @@ export default class AttackableUnit extends GameObject {
     super({ game, position, collisionRadius, visionRadius, teamId, id });
   }
 
+  update() {
+    // update Buffs
+    this.updateBuffs();
+
+    // update Stats
+    this.stats.update();
+
+    // move
+
+    // die
+  }
+
+  updateBuffs() {
+    this.buffs = this.buffs.filter(buff => !buff.toRemove);
+
+    // Combine the status effects of all the buffs
+    this._buffEffectsToEnable = 0;
+    this._buffEffectsToDisable = 0;
+
+    for (let buff of this.buffs) {
+      buff.update();
+
+      this._buffEffectsToEnable |= buff.statusFlagsToEnable;
+      this._buffEffectsToDisable |= buff.statusFlagsToDisable;
+    }
+
+    // If the effect should be enabled, it overrides disable.
+    this._buffEffectsToDisable &= ~this._buffEffectsToEnable;
+
+    this.setStatus(StatusFlags.None, true);
+  }
+
   setStatus(status, enabled) {
     let _statusBeforeApplyingBuffEfects = 0;
     if (enabled) _statusBeforeApplyingBuffEfects |= status;
@@ -26,5 +59,9 @@ export default class AttackableUnit extends GameObject {
       (_statusBeforeApplyingBuffEfects & ~this._buffEffectsToDisable) | this._buffEffectsToEnable;
 
     this.stats.updateActionState(status);
+  }
+
+  get isDead() {
+    return false;
   }
 }
