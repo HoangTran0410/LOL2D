bf25332264812a4580adef118d1ce928b317c08f
hoang.tran12 <hoang.tran12@mservice.com.vn>
2023-07-17 17:39:32 +0700

    ZedW - is this done ?

--- src/game/gameObject/spells/Zed_W.js
+++ src/game/gameObject/spells/Zed_W.js
@@ -5,14 +5,13 @@ import Spell from '../Spell.js';
 import SpellObject from '../SpellObject.js';
 import Champion from '../attackableUnits/Champion.js';
 import Dash from '../buffs/Dash.js';
-import TrailSystem from '../helpers/TrailSystem.js';
 
 export default class Zed_W extends Spell {
   image = AssetManager.getAsset('spell_zed_w');
   name = 'Phân Thân Bóng Tối (Zed_W)';
   description =
     'Tạo 1 phân thân lướt tới trước và đứng yên trong 5s, nó có thể bắt chước các kỹ năng bạn tung ra. Có thể tái kích hoạt kỹ nămg để đổi chỗ với phân thân.';
-  coolDown = 1000;
+  coolDown = 5000;
 
   zedWClone = null;
 
@@ -34,7 +33,7 @@ export default class Zed_W extends Spell {
       this.zedWClone.owner = this.owner;
       this.zedWClone.destination = to;
       this.zedWClone.onReachedDestination = () => {
-        this.currentCooldown = 0;
+        this.currentCooldown = 500;
       };
       this.game.addPlayer(this.zedWClone);
 
@@ -62,29 +61,36 @@ export class Zed_W_Clone extends Champion {
   lifeTime = 3000;
   age = 0;
 
-  onSomeoneCastSpell(spellInstance) {
+  onSomeoneCastSpell = spellInstance => {
     if (spellInstance instanceof Zed_W) return;
     if (spellInstance.owner != this.owner) return;
-    alert('cast');
     let spellClone = new spellInstance.constructor(this);
     spellClone.cast();
-    // this.game.eventManager.once(EventType.ON_CAST_SPELL, this.onSomeoneCastSpell.bind(this));
-  }
+  };
 
   onAdded() {
-    this.game.eventManager.once(EventType.ON_CAST_SPELL, this.onSomeoneCastSpell.bind(this));
+    this.game.eventManager.on(EventType.ON_CAST_SPELL, this.onSomeoneCastSpell);
+
+    let originSize = this.stats.size.baseValue;
+    this.stats.size.baseValue = 15;
+
+    let originVisionRadius = this.stats.visionRadius.baseValue;
+    this.stats.visionRadius.baseValue = 0;
 
     let dashBuff = new Dash(1000, this, this);
+    dashBuff.dashSpeed = 20;
+    dashBuff.cancelable = false;
     dashBuff.dashDestination = this.destination;
-    dashBuff.dashSpeed = 30;
     dashBuff.onReachedDestination = () => {
+      this.stats.size.baseValue = originSize;
+      this.stats.visionRadius.baseValue = originVisionRadius / 3;
       this.onReachedDestination?.();
     };
     this.buffs.push(dashBuff);
   }
 
   onRemoved() {
-    // this.game.eventManager.unsub(EventType.ON_CAST_SPELL, this.onSomeoneCastSpell.bind(this));
+    this.game.eventManager.unsub(EventType.ON_CAST_SPELL, this.onSomeoneCastSpell);
   }
 
   update() {
@@ -94,9 +100,21 @@ export class Zed_W_Clone extends Champion {
     if (this.age >= this.lifeTime) this.toRemove = true;
   }
 
+  draw() {
+    super.draw();
+
+    push();
+    //gray overlay
+    fill(50, 150);
+    noStroke();
+    circle(this.position.x, this.position.y, this.animatedValues.size);
+    pop();
+  }
+
   // un-targetable, immune to damage/heal, ...
   // TODO: implement targetable status instead of this
   addBuff() {}
   takeDamage() {}
   takeHeal() {}
+  drawHealthBar() {}
 }
